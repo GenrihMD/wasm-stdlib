354fd377389372e8698031039e83a3b22d257205
lubabelaya <lbelaya@stc-spb.ru>
2020-11-01 08:44:56 +0300

    Test clog

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,9 +1,11 @@
 // The entry file of your WebAssembly module.
 
+import * as console from  'bindings/console'
+
 export function add(a: i32, b: i32): i32 {
   return a + b;
 }
 
-export function retStr(): string {
-  return 'hello'
+export function sayHello(s: string): void {
+  console.log(null); // see Example 3
 }
